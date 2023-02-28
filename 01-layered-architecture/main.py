c0a1a64a49eddf9911145097770f93d978f383f3
fisher85 <79208085372@ya.ru>
2023-02-28 11:36:49 +0300

    Add minimal frontend

--- 01-layered-architecture/main.py
+++ 01-layered-architecture/main.py
@@ -1,40 +1,26 @@
 from fastapi import Body, FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from math import sqrt
 import uvicorn
 
 
 app = FastAPI()
+app.mount("/public", StaticFiles(directory="public"), name="public")
 
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Hello, world!</h1>
-        </body>
-        <script type="text/javascript">
-            function changeHeader() {
-                let h1 = document.querySelector('h1');
-                h1.innerHTML = '3 seconds have passed!';
-            }
-            setTimeout(changeHeader, 3000);
-        </script>
-    </html>
-    """
+@app.get("/")
+async def read_index():
+    return RedirectResponse(url="/public/index.html")
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: int):
-    return {"item_id": item_id, "q": q}
+@app.get("/square_root/{number}")
+async def square_root(number: float):
+    return sqrt(number)
 
 
 @app.post("/square_root")
-async def square_root(number: int = Body(embed=True)):
+async def square_root(number: float = Body(embed=True)):
     return sqrt(number)
 
 
