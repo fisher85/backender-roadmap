b12aa72819061c10b10b519020f24ef4c9cac9e3
ntdev asdasd123 <ntodev@ya.ru>
2023-04-07 09:22:56 +0300

    work ci, test

--- 01-layered-architecture/main.py
+++ 01-layered-architecture/main.py
@@ -24,5 +24,10 @@ async def square_root(number: float = Body(embed=True)):
     return sqrt(number)
 
 
+@app.post("/secret_func")
+async def secret_func(first: int = Body(embed=True), second: int = Body(embed=True)):
+    return {'answer': 'Bad input' if second < 0 else first >> second}
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="debug")
